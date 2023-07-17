c4ba75a15497af3eb2a2fd218f820afb35d7c6e1
Dalton Stevens <d.72.stevens@gmail.com>
2023-07-17 16:46:06 -0500

    added unit 2 project

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import Header from "./components/Header";
+import axios from "axios";
+import MovieScreen from "./components/MovieScreen";
+
+function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${
+          import.meta.env.VITE_APP_API_KEY
+        }&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <MovieScreen
+          watchlist={watchlist}
+          page={page}
+          setPage={setPage}
+          movieList={movieList}
+        />
+      </main>
+    </div>
+  );
+}
+
+export default App;
